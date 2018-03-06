434bab1cec63b49c16d81106e3d609a7e09367a6
Abigail Oliver <aoliver@vendasta.com>
2018-03-06 17:28:46 -0600

    - get angular material working - generate the main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatIconModule, MatToolbarModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { BlogComponent } from './blog/blog.component';
 
+
+export const routerConfig: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'blog',
+        component: BlogComponent
+    },
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavigationComponent,
+    BlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MatIconModule, RouterModule.forRoot(routerConfig), MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
